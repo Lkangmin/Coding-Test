49b56ff7e6484412d421a75113e5f4d9528e479d
LKM <kangmin7434@naver.com>
2022-08-29 18:51:45 +0900

    ADD: level 2

---
+++ Programmers/연습문제/42839.py
@@ -0,0 +1,20 @@
+from itertools import permutations
+
+def prime(a):
+    for i in range(2, int(a**0.5)+1):
+        if a%i:
+            continue
+        else:
+            return False
+    return True
+
+def solution(numbers):
+    answer = 0
+    total = set()
+    for i in range(1, len(numbers)+1):
+        total.update(set(map(int, map(''.join, permutations(numbers, i)))))
+
+    for j in total:
+        if prime(j) and j > 1:
+            answer += 1
+    return answer
